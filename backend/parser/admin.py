3405b849b8a8ae45d6718d9be46736a845443bf3
Fortunella <sherbakovdaniil6@gmail.com>
2023-07-26 23:16:44 +0300

    added product models customized product models in admin panel

--- backend/parser/admin.py
+++ backend/parser/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from parser.models import Product
+
+
 # Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Class for viewing transactions in admin panel"""
+
+    list_display = ('pk', 'title', 'price')
+    # list_filter = ('type',)
+    ordering = ('pk', 'title', 'price')
+    list_per_page = 30
+    search_fields = ('pk', 'title')
+    list_display_links = ('pk', 'title')
--- backend/parser/models.py
+++ backend/parser/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(verbose_name='title', max_length=50)
+    url_product = models.URLField(verbose_name='url')
+    url_photo = models.URLField(verbose_name='photo')
+    price = models.PositiveIntegerField(verbose_name='price', max_length=50)
+
+    def __str__(self):
+        return self.title
