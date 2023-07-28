258f6c2e3e975ab321663314621ec1261e3f8aad
Fortunella <sherbakovdaniil6@gmail.com>
2023-07-28 19:59:56 +0300

    Fixed parsing task

--- backend/parser/models.py
+++ backend/parser/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class Product(models.Model):
     """Product model"""
 
-    name = models.CharField(verbose_name='title', max_length=50)
-    url = models.URLField(verbose_name='url')
+    name = models.CharField(verbose_name='title', max_length=250)
+    url = models.URLField(verbose_name='url', max_length=500)
 
     def __str__(self):
         return self.name
