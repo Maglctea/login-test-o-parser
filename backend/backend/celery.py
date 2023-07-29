836e47817a210c934277035b7f9df6de0c752375
Fortunella <sherbakovdaniil6@gmail.com>
2023-07-29 17:57:41 +0300

    Refactor code

--- backend/backend/celery.py
+++ backend/backend/celery.py
@@ -7,10 +7,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 app = Celery('backend')
 
-# Using a string here means the worker doesn't have to serialize
-# the configuration object to child processes.
-# - namespace='CELERY' means all celery-related configuration keys
-#   should have a `CELERY_` prefix.
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django apps.
--- backend/parser/urls.py
+++ backend/parser/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from setuptools import namespaces
 
 from parser.views import ListProductsView, DetailProductsView
 
